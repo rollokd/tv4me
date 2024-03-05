5f5c170f8a3893a5dcfa8e1a992b42f097e2947f
Rollo Kennedy-Dyson <rollokd@gmail.com>
2024-03-05 13:57:17 +0000

    feat: updated .gitignore

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import dbConnect from "./lib/db";
 import Link from "next/link";
 import { getUser } from "./lib/users";
+import { User } from "./lib/definitions";
 
 export default async function Home() {
   const id = "65e0df64e483a7bd7222c535";
-  const user = await getUser(id);
+  const user: User = await getUser(id);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Hello</div>
