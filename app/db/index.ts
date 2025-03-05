61b3877c82d6ef213398eaf78ee4a0642ead6035
Rollo Kennedy-Dyson <rollokd@gmail.com>
2025-03-05 22:33:46 +0000

    fix: env import

--- app/db/index.ts
+++ app/db/index.ts
@@ -1,7 +1,7 @@
 // src/db.ts
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
-import "./envConfig.ts";
+import "../../envConfig";
 
 const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle({ client: sql });
